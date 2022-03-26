52a0f537fd4df01adf06f931ae4a6f08b14bfedb
SomewhatRealAlex <alex.zhang0023@gmail.com>
2022-03-25 19:07:41 -0500

    updated

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -13,3 +13,37 @@ function checkNavbar() {
         document.getElementById("navbar").style.webkitBoxShadow = "none";
     }
 }
+
+function gotoDiv(div) {
+    document.getElementById(div).scrollIntoView();
+}
+
+function getResources(clicked, name) {
+    for(let i = 0; i < document.getElementsByClassName("c-rlt-button").length; i++) {
+        document.getElementsByClassName("c-rlt-button")[i].classList.remove("c-r-selected");
+    }
+
+    clicked.classList.add("c-r-selected");
+    document.getElementById("resourcesheader").innerText = clicked.innerText + " Resources";
+
+    document.getElementById("resourcesbody").innerHTML = "Loading...";
+    
+    //connect to resource api with name as query and parse output
+    //then add resources
+}
+
+function showQuizScores() {
+    document.getElementById("showquizbtn").classList.add("c-sc-selected");
+    document.getElementById("showcompbtn").classList.remove("c-sc-selected");
+
+    document.getElementById("quizscoresarea").style.display = "block";
+    document.getElementById("compscoresarea").style.display = "none";
+}
+
+function showCompScores() {
+    document.getElementById("showcompbtn").classList.add("c-sc-selected");
+    document.getElementById("showquizbtn").classList.remove("c-sc-selected");
+
+    document.getElementById("quizscoresarea").style.display = "none";
+    document.getElementById("compscoresarea").style.display = "block";
+}
